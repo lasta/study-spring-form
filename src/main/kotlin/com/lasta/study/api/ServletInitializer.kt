9d9955c5a295ec08ba9dd1aec04e9945e6da76e5
Atsushi KOSHIKIZAWA <unconfigured@null.spigotmc.org>
2018-12-22 16:16:28 +0900

    Initialized by spring-initializr

---
+++ src/main/kotlin/com/lasta/study/api/ServletInitializer.kt
@@ -0,0 +1,13 @@
+package com.lasta.study.api
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(StudySpringFormApplication::class.java)
+    }
+
+}
+
---
+++ src/main/kotlin/com/lasta/study/api/StudySpringFormApplication.kt
@@ -0,0 +1,12 @@
+package com.lasta.study.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudySpringFormApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudySpringFormApplication>(*args)
+}
+
